121e4d002ff90459d28a6dbdb5c361c45bdc1dd1
Corinto <salazar72390@gmail.com>
2023-08-07 16:28:25 +0800

    v2

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,4 +1,4 @@
-const options = ['rock', 'paper', 'scissors'];
+const options = ["rock", "paper", "scissors"];
 let playerScore = 0;
 let computerScore = 0;
 const history = [];
@@ -10,56 +10,50 @@ function computerPlay() {
 
 function playRound(playerSelection) {
   const computerSelection = computerPlay();
-  let roundResult = '';
+  let roundResult = "";
 
   if (playerSelection === computerSelection) {
     roundResult = "It's a tie!";
   } else if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') ||
-    (playerSelection === 'paper' && computerSelection === 'rock') ||
-    (playerSelection === 'scissors' && computerSelection === 'paper')
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
   ) {
-    roundResult = '<span style="color:green">You win! ' + playerSelection + ' beats ' + computerSelection + '.</span>';
+    roundResult = `<span class="win">You win! ${playerSelection} beats ${computerSelection}.</span>`;
     playerScore++;
   } else {
-    roundResult = '<span style="color:red">You lose! ' + computerSelection + ' beats ' + playerSelection + '.</span>';
+    roundResult = `<span class="lose">You lose! ${computerSelection} beats ${playerSelection}.</span>`;
     computerScore++;
   }
 
-  let result = playerScore + ' - ' + computerScore;
-  if (playerScore > computerScore) {
-    result = '<span style="color:green">' + result + '</span>';
-  } else if (computerScore > playerScore) {
-    result = '<span style="color:red">' + result + '</span>';
-  } else {
-    result = '<span>' + result + '</span>';
-  }
+  updateResult(roundResult);
+  updateHistory(roundResult);
+  updateScoreboard();
+}
 
-  document.getElementById('result').innerHTML = roundResult;
+function updateResult(roundResult) {
+  document.getElementById("result").innerHTML = roundResult;
+}
 
-  // Update history
-  history.push({ roundResult, result });
+function updateHistory(roundResult) {
+  history.push({ roundResult, result: `${playerScore} - ${computerScore}` });
   if (history.length > 10) {
     history.shift();
   }
-  let historyList = '';
-  for (let i = 0; i < history.length; i++) {
-    historyList += '<li>' + history[i].roundResult + ' ' + history[i].result + '</li>';
-  }
-  document.getElementById('history').innerHTML = historyList;
+  document.getElementById("history").innerHTML = history
+    .map((item) => `<li>${item.roundResult} <span>${item.result}</span></li>`)
+    .join("");
+}
 
-  // Update scoreboard
-  document.getElementById('player-score').innerHTML = playerScore;
-  document.getElementById('computer-score').innerHTML = computerScore;
+function updateScoreboard() {
+  document.getElementById("player-score").innerHTML = playerScore;
+  document.getElementById("computer-score").innerHTML = computerScore;
 
   if (playerScore === 5 || computerScore === 5) {
-    let finalResult = '';
-    if (playerScore > computerScore) {
-      finalResult = '<span style="color:green">You won!</span>';
-    } else {
-      finalResult = '<span style="color:red">You lost!</span>';
-    }
-    document.getElementById('result').innerHTML = finalResult + ' Final score: ' + result;
+    let finalResult = playerScore > computerScore ? "You won!" : "You lost!";
+    document.getElementById("result").innerHTML = `<span class="${
+      playerScore > computerScore ? "win" : "lose"
+    }">${finalResult} Final score: ${playerScore} - ${computerScore}</span>`;
     playerScore = 0;
     computerScore = 0;
     history.length = 0;
